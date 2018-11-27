a03441d31a125a9146b73620c5eae14a494ed07f
Pierre Vignolo <36164748+PierreVignolo@users.noreply.github.com>
2018-11-27 11:32:37 +0100

    feat: Options

---
+++ src/Controller/Admin/AdminTagController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Tag;
+use App\Form\TagType;
+use App\Repository\TagRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/option")
+ */
+class AdminTagController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin.option.index", methods="GET")
+     * @param TagRepository $tagRepository
+     * @return Response
+     */
+    public function index(TagRepository $tagRepository): Response
+    {
+        return $this->render('tag/index.html.twig', ['tags' => $tagRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="tag_new", methods="GET|POST")
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $tag = new Tag();
+        $form = $this->createForm(TagType::class, $tag);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($tag);
+            $em->flush();
+
+            return $this->redirectToRoute('tag_index');
+        }
+
+        return $this->render('tag/new.html.twig', [
+            'tag' => $tag,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="tag_edit", methods="GET|POST")
+     * @param Request $request
+     * @param Tag $tag
+     * @return Response
+     */
+    public function edit(Request $request, Tag $tag): Response
+    {
+        $form = $this->createForm(TagType::class, $tag);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('tag_edit', ['id' => $tag->getId()]);
+        }
+
+        return $this->render('tag/edit.html.twig', [
+            'tag' => $tag,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="tag_delete", methods="DELETE")
+     * @param Request $request
+     * @param Tag $tag
+     * @return Response
+     */
+    public function delete(Request $request, Tag $tag): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$tag->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($tag);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('tag_index');
+    }
+}
